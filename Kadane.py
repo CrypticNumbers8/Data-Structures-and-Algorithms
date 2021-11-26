d372f4496e12104c24be6e8edbb89eee2c17ead7
Aniket Karna <57065053+CrypticNumbers8@users.noreply.github.com>
2021-11-25 20:14:25 -0700

    Kadane_updated

--- Kadane.py
+++ Kadane.py
@@ -1,4 +1,5 @@
 #Kadane's Algorithm:
+#Time Complexity: O(N)
 
 def max_sum_subarray_not_all_negative(l1):
 
@@ -17,7 +18,22 @@ def max_sum_subarray_not_all_negative(l1):
 	return max_sum
 
 def max_sum_subarray_all_negative(l2):
-	return len(l2)
+
+	current_sum = 0
+	max_sum = l2[0]
+
+	for i in range(len(l2)):
+		current_sum += l2[i]
+
+		if current_sum>max_sum:
+			max_sum = current_sum
+
+		if current_sum<0:
+			current_sum = 0
+
+	return max_sum
+
+
 
 if __name__ == '__main__':
 
@@ -26,5 +42,6 @@ if __name__ == '__main__':
 
 	w = max_sum_subarray_not_all_negative(a)
 	x = max_sum_subarray_all_negative(b)
+
 	print(w)
 	print(x)
